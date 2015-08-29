0f657544cbcd16930689b674749ded10aca9f22b
Anderson B. Sensolo <anderson.sensolo@gmail.com>
2015-08-29 17:21:11 -0300

    Primeiro Commit

---
+++ src/java/model/Equipe.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author anderson
+ */
+public class Equipe {
+   
+    private int id;
+    private String nome;
+    private Date dataFundacao;
+    private String email;
+    private String enderecoWeb;
+    private String fone;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Date getDataFundacao() {
+        return dataFundacao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEnderecoWeb() {
+        return enderecoWeb;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDataFundacao(Date dataFundacao) {
+        this.dataFundacao = dataFundacao;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setEnderecoWeb(String enderecoWeb) {
+        this.enderecoWeb = enderecoWeb;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+    
+}
